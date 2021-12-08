954051d76c6cbbde6c23657d39ddc20aec292345
Caglar <caglarsulun@hotmail.com>
2021-12-07 19:16:08 -0500

    v2.0

--- index.js
+++ index.js
@@ -1,70 +1,115 @@
-fetch("https://www.boredapi.com/api/activity")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (object) {
-    console.log(object);
-  })
+let currentActivity = " ";
+let currentKey = " ";
+const favButton = document.querySelector("#favorite-button");
+const favS = document.querySelector("#favorite");
 
-//test
-
-
-  document.addEventListener("DOMContentLoaded", () => {
-    let form = document.querySelector("#create-task-form")
-    
-    
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-  
-    
-      buildToDo(e.target.newTaskDescription.value)
-      
-      form.reset()
+//calls random activity page load, adds event listener for search button & favorites
+document.addEventListener("DOMContentLoaded", () => {
+    randomActivity();
+    document.querySelector("#activity-search").addEventListener("submit", (e) => {
+        e.preventDefault();
+        const activityType = document.querySelector("#activity-type").value;
+        activitySearch(activityType);
     })
-  });
-  
+    favListen();
+});
 
+function randomActivity() {
+    fetch("https://www.boredapi.com/api/activity")
+        .then((resp) => resp.json())
+        .then((data) => {
+            const { activity, key } = data;
+            currentActivity = activity;
+            currentKey = key;
+            document.querySelector("#activity-description").textContent = currentActivity;
+            console.log("current activity: " + currentActivity);
+        })
+};
 
-  function buildToDo(todo){
-    let p = document.createElement('p')
-    let btn = document.createElement('button')
-    btn.addEventListener('click', handleDelete)
-    btn.textContent = 'x'
-    p.textContent = `${todo} `
-    p.appendChild(btn)
-    console.log(p)
-    document.querySelector("#tasks").appendChild(p)
+//searches for activity by type
+function activitySearch(activityType) {
+    if (activityType == "random") {
+        randomActivity();
     }
-  
-  
-    function handleDelete(e){
-        e.target.parentNode.remove()
-    }
-
-
- 
-
-
-
-    document.addEventListener("DOMContentLoaded", function () {
-        addImage();
-                
-      });
-      
-      function addImage() {
-        fetch("https://www.boredapi.com/api/activity")
-          .then((resp) => resp.json())
-          .then((data) => {
-            //console.log(data);
-            document.querySelector("#commentsList").textContent = data.activity;
-            document.querySelector("#commentsList > li").textContent = data.type;
-      
-            
-      
-            data.comments.forEach((comment) => {
-              const li = document.createElement("li");
-              li.textContent = comment.content;
-              document.querySelector("#commentsList > li").append(li);
+    else {
+        fetch(`http://www.boredapi.com/api/activity?type=${activityType}`)
+            .then((resp) => resp.json())
+            .then((data) => {
+                const { activity, key } = data;
+                currentActivity = activity;
+                currentKey = key;
+                document.querySelector("#activity-description").textContent = activity;
+                console.log("current activity: " + currentActivity);
             });
-          });
-      }
+    }
+    clearFav();
+};
+
+//to change heart/span text color and call create favorite or remove favorite
+const favActivity = () => {
+    if (favButton.textContent == '♡' & currentActivity !== " ") {
+        favButton.textContent = '♥';
+        favS.textContent = "This is a favorite activity!"
+        favS.parentNode.className += " favorited"
+        console.log("favorited: " + currentActivity);
+        createFav();
+    }
+    else if (favButton.textContent == '♥') {
+        clearFav();
+        unFav();
+    };
+};
+
+//adds activity to favorite-activities-list, adds event listener to click x and remove.
+function createFav() {
+    const fav = document.querySelector(".favorite-activities-list");
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const btn = document.createElement("button");
+    span.textContent = currentActivity;
+    btn.textContent = "✕";
+    btn.addEventListener("click", handleDelete)
+    fav.append(li);
+    li.append(span)
+    span.append(btn);
+};
+
+//clears the favorite button
+function clearFav() {
+    favS.textContent = "Mark this activity as a favorite!"
+    favButton.textContent = '♡';
+    favS.parentNode.className -= " favorited"
+};
+
+//clicking heart removes favorite from list
+function unFav () {
+    const favList = document.querySelectorAll("li");
+    for (let i = 0; i < favList.length; i++) {
+        if (favList[i].textContent == currentActivity + '✕'){
+            favList[i].remove();
+        }
+    }
+}
+
+// event listener that allows heart button to work
+function favListen() {
+    document.querySelector("#favorite-button").addEventListener("click", () => {
+        favActivity();
+    })
+}
+
+//deletes favorite by clicking X and clears favorite if it is the current activity
+function handleDelete(e){
+    if (e.target.parentNode.textContent == currentActivity + '✕'){
+        clearFav()
+    };
+    console.log(e);
+    e.target.parentNode.parentNode.remove();
+}
+
+/* !!still in progress!! to build activity object for json 
+function (activity, key) {
+    "Activity" : activity;
+    "Key": key;
+}
+*/
